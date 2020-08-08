12f9dc5896bfa6659ca50d1b00f87bfe47d49238
chrishoen <chris123hoen@gmail.com>
2020-08-08 12:43:06 -0400

    adding acmproc

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -2,15 +2,15 @@
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_ShareLib _target)
+function(my_lib_import_ShareLibACM _target)
 
-   target_link_libraries(${_target} ShareLib)
+   target_link_libraries(${_target} ShareLibACM)
 
 endfunction()
 
-function(my_inc_import_ShareLib _target)
+function(my_inc_import_ShareLibACM _target)
 
-   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:ShareLib,INTERFACE_INCLUDE_DIRECTORIES>)
+   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:ShareLibACM,INTERFACE_INCLUDE_DIRECTORIES>)
 
 endfunction()
 
---
+++ acmproc/MainInit.cpp
@@ -0,0 +1,70 @@
+#include "stdafx.h"
+
+#include "risThreadsProcess.h"
+
+#include "smShare.h"
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Initialize program resources.
+
+void main_initialize(int argc,char** argv)
+{
+   printf("acmproc Program********************************************BEGIN\n");
+   printf("acmproc Program********************************************BEGIN\n");
+   printf("acmproc Program********************************************BEGIN\n\n");
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // Initialize print facility.
+
+   // Initialize print.
+   Prn::resetPrint();
+   Prn::useConsole(1);
+   Prn::initializePrint();
+
+   // Initialize print filters.
+   Prn::setFilter(Prn::View11, true,  1);
+   Prn::setFilter(Prn::View12, false, 1);
+   Prn::setFilter(Prn::View13, false, 1);
+   Prn::setFilter(Prn::View14, false, 1);
+
+   Prn::setFilter(Prn::Show1, true);
+   Prn::setFilter(Prn::Show2, true);
+   Prn::setFilter(Prn::Show3, false);
+   Prn::setFilter(Prn::Show4, false);
+   Prn::setFilter(Prn::Show5, false);
+   Prn::setFilter(Prn::Show6, false);
+
+   //***************************************************************************
+   //***************************************************************************
+   //***************************************************************************
+   // initialize.
+
+   // Initialize shared memory.
+   SM::initializeShare();
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
+// Finalize program resourcs.
+
+void main_finalize()
+{
+   // Finalize shared memory.
+   SM::finalizeShare();
+
+   // Finalize print facility.
+   Prn::finalizePrint();
+
+   // Done.
+   printf("\n");
+   printf("acmproc Program********************************************END\n\n");
+}
+
+//******************************************************************************
+//******************************************************************************
+//******************************************************************************
