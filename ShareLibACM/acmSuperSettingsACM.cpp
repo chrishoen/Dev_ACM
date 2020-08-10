16b63772068fd767c9c5fbc035fc2acd25d194a7
chrishoen <chris123hoen@gmail.com>
2020-08-10 11:59:42 -0400

    more

--- ShareLibACM/acmSuperSettingsACM.cpp
+++ ShareLibACM/acmSuperSettingsACM.cpp
@@ -90,26 +90,73 @@ void SuperSettingsACM::initialize()
 
 void SuperSettingsACM::show(int aPF)
 {
+	// Low power threshold.
 	Prn::print(aPF, "LowPowerThresh          %10.2f %10.2f %10s",
 	   mTxLowPowerThresh_pct,
 	   mRxLowPowerThresh_pct,
 		asString_Qx(mQxLowPowerThresh_pct));
 
+	// Low power alarm.
 	Prn::print(aPF, "LowPowerAlarmEnable     %10s %10s %10s",
 		my_string_from_bool(mTxLowPowerAlarmEnable),
 		my_string_from_bool(mRxLowPowerAlarmEnable),
 		asString_Qx(mQxLowPowerAlarmEnable));
 
+	// High power threshold.
 	Prn::print(aPF, "HighPowerThresh         %10.2f %10.2f %10s",
 		mTxHighPowerThresh_pct,
 		mRxHighPowerThresh_pct,
 		asString_Qx(mQxHighPowerThresh_pct));
 
+	// High power alarm.
 	Prn::print(aPF, "HighPowerAlarmEnable    %10s %10s %10s",
 		my_string_from_bool(mTxHighPowerAlarmEnable),
 		my_string_from_bool(mRxHighPowerAlarmEnable),
-	asString_Qx(mQxHighPowerAlarmEnable));
+	   asString_Qx(mQxHighPowerAlarmEnable));
 
+	// Gain.
+	Prn::print(aPF, "Gain                    %8d %1d %8d %1d %10s",
+		mTxForwardGain,
+		mTxReverseGain,
+		mRxForwardGain,
+		mRxReverseGain,
+		asString_Qx(mQxGain));
+	return;
+
+	// Latch alarm enable.
+	mTxLatchAlarmEnable = false;
+	mRxLatchAlarmEnable = false;
+	mQxLatchAlarmEnable = 0;
+
+	// Power up alarm enable.
+	mTxPowerUpAlarmEnable = false;
+	mRxPowerUpAlarmEnable = false;
+	mQxPowerUpAlarmEnable = 0;
+
+	// Relay energize on power alarm enable.
+	mTxRelayOnPowerAlarmEnable = false;
+	mRxRelayOnPowerAlarmEnable = false;
+	mQxRelayOnPowerAlarmEnable = 0;
+
+	// Relay energize on VSWR alarm enable.
+	mTxRelayOnVSWRAlarmEnable = false;
+	mRxRelayOnVSWRAlarmEnable = false;
+	mQxRelayOnVSWRAlarmEnable = 0;
+
+	// Check VSWR on zero power.
+	mTxCheckVSWROnZeroPower = false;
+	mRxCheckVSWROnZeroPower = false;
+	mQxCheckVSWROnZeroPower = 0;
+
+	// PTT delay.
+	mTxPTTDelay = 0.0;
+	mRxPTTDelay = 0.0;
+	mQxPTTDelay = 0;
+
+	// PTT alarm.
+	mTxPTTAlarmEnable = false;
+	mRxPTTAlarmEnable = false;
+	mQxPTTAlarmEnable = 0;
 }
 
 //******************************************************************************
