8ad087081684c31e6952f671b7c9bb88da030e19
chrishoen <chris123hoen@gmail.com>
2020-08-13 09:22:54 -0400

    variable 1 added a simplification

--- CommonLib/acmCommSeqThread_01_LowPowerThresh_pct.cpp
+++ CommonLib/acmCommSeqThread_01_LowPowerThresh_pct.cpp
@@ -32,10 +32,10 @@ namespace ACM
 
 void CommSeqThread::txrxLowPowerThresh_pct(bool aTxFlag)
 {
-//***************************************************************************
-//***************************************************************************
-//***************************************************************************
-// Send a command to the acm.
+	//***************************************************************************
+	//***************************************************************************
+	//***************************************************************************
+	// Send a command to the acm.
 
 	// Do this first.
 	ACM::SuperSettingsACM* tS = &SM::gShare->mSuperSettingsACM;
@@ -62,20 +62,20 @@ void CommSeqThread::txrxLowPowerThresh_pct(bool aTxFlag)
 	// Send the command.
 	sendString(tTxString);
 
-	// Wait for the receive acknowledgement notification.
+	// Wait for the receive response notification.
 	mNotify.wait(cCmdAckTimeout);
 
-//***************************************************************************
-//***************************************************************************
-//***************************************************************************
-// Process the response from the acm.
+	//***************************************************************************
+	//***************************************************************************
+	//***************************************************************************
+	// Process the response from the acm.
 
 	// Read the receive string from the queue.
 	std::string* tRxString = mRxStringQueue.tryRead();
 	if (tRxString == 0)
 	{
 		Prn::print(Prn::View11, "RxQueue EMPTY");
-		tS->mQxLowPowerThresh_pct = cQx_Ack;
+		tS->mQxLowPowerThresh_pct = cQx_Nak;
 		delete tRxString;
 		return;
 	}
@@ -113,22 +113,11 @@ void CommSeqThread::txrxLowPowerThresh_pct(bool aTxFlag)
 	// Convert.
 	tRxLowPowerThresh_pct = 100.0 * tV / 26214.0;
 
-	// If rx only then done. Copy the temp to the settings and exit.
-	if (!aTxFlag)
-	{
-		tS->mRxLowPowerThresh_pct = tRxLowPowerThresh_pct;
-		tS->mQxLowPowerThresh_pct = cQx_Ack;
-		Prn::print(Prn::View21, "LowPowerThresh_pct %s %.2f",
-			asString_Qx(tS->mQxLowPowerThresh_pct), tRxLowPowerThresh_pct);
-		delete tRxString;
-		return;
-	}
-
 	// Compare the tx and rx variables.
-	if (my_closeto(tTxLowPowerThresh_pct, tRxLowPowerThresh_pct, 0.01))
+	if (!aTxFlag || my_closeto(tTxLowPowerThresh_pct, tRxLowPowerThresh_pct, 0.01))
 	{
-		// If ok then copy the temp to the rx variable and set the
-		// qx ack code for an ack. 
+		// If rx only or compare ok then copy the temp to the rx variable
+		// and set the qx ack code for an ack. 
 		tS->mRxLowPowerThresh_pct = tRxLowPowerThresh_pct;
 		tS->mQxLowPowerThresh_pct = cQx_Ack;
 	}
