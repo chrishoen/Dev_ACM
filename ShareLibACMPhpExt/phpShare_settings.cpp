3d269e72bbeea85489ea638a99865c13db53c065
chrishoen <chris123hoen@gmail.com>
2020-08-16 12:12:27 -0400

    more

--- ShareLibACMPhpExt/phpShare_settings.cpp
+++ ShareLibACMPhpExt/phpShare_settings.cpp
@@ -27,45 +27,60 @@ Php::Value share_get_SuperSettingsACM_Json()
 
 void share_set_SettingsLowPowerThresh_pct_float(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxLowPowerThresh_pct = (float)(double)aP[0];
 }
 void share_set_SettingsLowPowerAlarmEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxLowPowerAlarmEnable = (bool)aP[0];
 }
 void share_set_SettingsHighPowerThresh_pct_float(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxHighPowerThresh_pct = (float)(double)aP[0];
 }
 void share_set_SettingsHighPowerAlarmEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxHighPowerAlarmEnable = (bool)aP[0];
 }
 void share_set_SettingsVSWRTrigger_int(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxVSWRTrigger = (int)aP[0];
 }
 void share_set_SettingsVSWRAlarmEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxVSWRAlarmEnable = (bool)aP[0];
 }
 void share_set_SettingsGain_int_int(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxForwardGain = (int)aP[0];
+   SM::gShare->mSuperSettingsACM.mTxReverseGain = (int)aP[1];
 }
 void share_set_SettingsLatchAlarmEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxLatchAlarmEnable = (bool)aP[0];
 }
 void share_set_SettingsPowerUpAlarmEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxPowerUpAlarmEnable = (bool)aP[0];
 }
 void share_set_SettingsRelayOnPowerEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxRelayOnPowerEnable = (bool)aP[0];
 }
 void share_set_SettingsRelayOnVSWREnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxRelayOnVSWREnable = (bool)aP[0];
 }
 void share_set_SettingsCheckVSWROnZeroEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxCheckVSWROnZeroEnable = (bool)aP[0];
 }
 void share_set_SettingsPTTDelay_sec_float(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxPTTDelay_sec = (float)(double)aP[0];
 }
 void share_set_SettingsPTTAlarmEnable_bool(Php::Parameters& aP)
 {
+   SM::gShare->mSuperSettingsACM.mTxPTTAlarmEnable = (bool)aP[0];
 }
 
 //******************************************************************************
